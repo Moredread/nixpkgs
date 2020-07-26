ad83e10b0074e48ac81de41020aac6f0c660fbbd
André-Patrick Bubel <code@andre-bubel.de>
2020-07-26 18:28:27 +0200

    --wip-- [skip ci]

--- pkgs/tools/security/sops/default.nix
+++ pkgs/tools/security/sops/default.nix
@@ -1,17 +1,23 @@
-{ stdenv, buildGoModule, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub, fetchpatch }:
 
 buildGoModule rec {
   pname = "sops";
   version = "3.6.0";
 
   src = fetchFromGitHub {
-    rev = "v${version}";
+    rev = "5f7d32404639855ee4f35f187714d71fc43aa4fa";
     owner = "mozilla";
     repo = pname;
     sha256 = "01skk6vdfki4a88z0snl1pby09im406qhnsfa0d2l8gp6nz8pq6j";
   };
 
-  vendorSha256 = "0475y95qma5m346ng898n80xv2rxzndx89c9ygjcvjs513yzcba2";
+  patches = [ (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/mozilla/sops/pull/688.patch";
+      sha256 = "132cfaf20j1vip48gyx0lvwk4r54cjbfxb7gpxfziq4x86bgqjn6";
+    })
+  ];
+
+  vendorSha256 = "1s04dzfivxbw9hxlma9w7qs4vmdca57kl5b55y6id3xk7xq17gxc";
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/mozilla/sops";
