e18d8ef9912edf5cb7cb954917f698e563a82104
Eric Drechsel <eric@pdxhub.org>
2023-02-26 22:01:36 -0800

    WIP gargoyle: 2019.1.1 -> 2023.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36764,7 +36764,7 @@ with pkgs;
 
   garden-of-coloured-lights = callPackage ../games/garden-of-coloured-lights { allegro = allegro4; };
 
-  gargoyle = callPackage ../games/gargoyle {
+  gargoyle = qt6Packages.callPackage ../games/gargoyle {
     inherit (darwin) cctools;
   };
 
